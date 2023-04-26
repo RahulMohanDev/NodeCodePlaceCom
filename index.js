a403c194e2bdae9f67707f5e5583ac6eaeab4a63
Rahul Mohan <rahul.mohan@exxat.com>
2023-04-26 20:24:21 +0530

    example with document embedding

--- index.js
+++ index.js
@@ -12,7 +12,6 @@
 // exports.b = 42;
 // exports.c = 99;
 
-
 // const cat ={
 //     name: 'cat',
 //     age: 3,
@@ -31,7 +30,6 @@
 //     name: 'test',
 // }
 
-
 // // default export
 // export default dog;
 // // normal export
@@ -40,8 +38,7 @@
 
 // import http from 'http';
 
-
-// api/v1/employees GET 
+// api/v1/employees GET
 // api/v1/managers  GET
 // api/v1/students  GET
 // api/v1/teachers  GET
@@ -49,20 +46,20 @@
 // const server = http.createServer((req, res) => {
 //     if(req.method === 'GET'){
 //         // if(req.url === '/'){
-      
+
 //         // res.write('hello world \n');
 //         // res.write('hi I am still there \n');
 //         // res.write('hi I am still there \n');
 //         // res.write('hi I am still there \n');
 //         // res.end();
-//         // } else if (req.url === '/about'){ 
+//         // } else if (req.url === '/about'){
 //         //     res.write('about page \n');
 //         //     res.end();
 //         // }
 //         if(req.url === '/api/v1/employees'){
 //             res.write('employees \n');
 //             res.end();
-//         } else if (req.url === '/api/v1/managers'){ 
+//         } else if (req.url === '/api/v1/managers'){
 //             res.write('managers \n');
 //             res.end();
 //         } else if (req.url === '/api/v1/students'){
@@ -89,8 +86,6 @@
 //     console.log('listening on port 4505')
 // });
 
-
-
 // import http from 'http';
 
 // const server = http.createServer((req, res) => {
@@ -103,13 +98,12 @@
 
 // server.listen(4505,()=>{});
 
-
 // todo list
-// GET 
+// GET
 // /todos
- //  {
- //    "todos": ['test' ,'test2',"teach at newton school"]
- //  }
+//  {
+//    "todos": ['test' ,'test2',"teach at newton school"]
+//  }
 
 // GET
 // /todos/3
@@ -120,54 +114,172 @@
 // POST
 // /todos
 // {
-    //     "todo": "teach at newton school"
+//     "todo": "teach at newton school"
 // }
 
 // DELETE
 // /todos/1
 
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import todoRoutes from './routes/todo/todo.js'
+import path from 'path'
+import mongoose, { Mongoose } from 'mongoose'
 
-import dotenv from 'dotenv';
-import express from "express";
-import cors  from 'cors';
-import todoRoutes from './routes/todo/todo.js';
-import path from 'path';
-import mongoose from 'mongoose';
+dotenv.config()
 
-dotenv.config();
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+const db = mongoose.connection
 
-console.log(process.env.MONGO_URL);
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true})
-const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
 
-db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to database'))
 
-db.once('open', () => console.log('connected to database'));
+console.log(process.argv[0])
+console.log(process.argv[1])
 
-console.log(process.argv[0]);
-console.log(process.argv[1]);
-
-const __dirname = path.dirname(process.argv[1]);
-
-
-
-const app = express();
+const __dirname = path.dirname(process.argv[1])
 
 
-console.log(path.join(__dirname,'public'));
+const messages = mongoose.Schema({
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: true,
+  },
+  isRead: {
+    type: Boolean,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+    max: 10,
+  },
+  attachments: [
+    {
+      typeOfDocument: {
+        type: String,
+        enum: ['pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx'],
+      },
+      fileName: String,
+      createdAt: Date,
+    },
+  ],
+  typeOfMessage: {
+    type: String,
+    enum: ['text', 'image', 'video', 'audio'],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v < Date.now()
+      },
+    },
+  },
+})
 
+const Message = mongoose.model('Messages', messages)
 
-app.use(express.static(path.join(__dirname, 'potato')));
+const app = express()
 
+console.log(path.join(__dirname, 'public'))
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'potato')))
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
 
-app.use('/todos', todoRoutes)
+app.use(express.urlencoded({ extended: true }))
 
+// app.use('/todos', todoRoutes)
 
+app.get('/api/v1/messages', (req, res) => {
+  const page = parseInt(req.query.page) || 1
+  const filterByMessage = req.query.q || ''
+  const limit = 3
+  const start = (page - 1) * limit
+  Message.find({
+    message: {
+      $regex: filterByMessage,
+      $options: 'i',
+    },
+  })
+    .skip(start)
+    .limit(limit)
+    .then((messages) => {
+      res.json(messages)
+    })
+})
 
-console.log(process.env.PORT);
-app.listen(process.env.PORT||8080,()=>{});
+app.post('/api/v1/messages', (req, res) => {
+  const dateAndTime = new Date(req.body.createdAt)
 
+  const attachments = req.body.attachments.map((attachment) => {
+    return {
+      typeOfDocument: attachment.typeOfDocument,
+      fileName: attachment.fileName,
+      createdAt: new Date(),
+    }
+  })
+
+  const message = new Message({
+    from: req.body.from,
+    to: req.body.to,
+    isRead: false,
+    attachments: attachments,
+    message: req.body.message,
+    typeOfMessage: req.body.typeOfMessage,
+    createdAt: dateAndTime,
+  })
+  message
+    .save()
+    .then((message) => {
+      res.json(message)
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        // Handle validation errors
+        console.log(err.errors)
+        res.status(400).json(err.errors)
+      } else {
+        // Handle other errors
+        console.log(err)
+      }
+    })
+})
+
+// query first
+// app.put('/api/v1/messages/:id', (req, res) => {
+//     Message.findById(req.params.id).then((message) => {
+//         message.isRead = true;
+//         message.save().then((message) => {
+//             res.json(message);
+//         });
+//     });
+// })
+
+// update first
+app.patch('/api/v1/messages/:id', (req, res) => {
+  Message.updateOne(
+    { _id: req.params.id },
+    {
+      isRead: true,
+    },
+  ).then((message) => {
+    res.json(message)
+  })
+})
+
+console.log(process.env.PORT)
+app.listen(process.env.PORT || 8080, () => {})
